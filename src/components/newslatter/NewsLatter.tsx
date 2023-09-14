381e626d752b20066c4b4770fb252c1b6d0d751d
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-09-14 23:16:14 +0700

    refactor: remove text

--- src/components/newslatter/NewsLatter.tsx
+++ src/components/newslatter/NewsLatter.tsx
@@ -103,10 +103,6 @@ export const NewsLatter = () => {
   );
 };
 /*
-
-
-  Join 60,000+ product managers receiving monthly updates on:
-
   Product discovery and building what matters
   Measuring to ensure updates are a success
   And much more!
