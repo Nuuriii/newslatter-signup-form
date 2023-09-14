b7309cb5be1d08511022197d6170c7a32c0308cf
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-09-14 23:11:30 +0700

    refactor: change text

--- src/components/newslatter/NewsLatter.tsx
+++ src/components/newslatter/NewsLatter.tsx
@@ -50,7 +50,7 @@ export const NewsLatter = () => {
             <MobileImage src={MobileImg} />
           </MobileImgWrapper>
           <Information>
-            <Title>Stay updated!</Title>
+            <Title>tay updated!</Title>
             <Text>
               Join 60,000+ product managers receiving monthly updates on:
             </Text>
